eb6f16240c3f3b4d65ef5e2445eb2d9aa82e21ca
kytnacode <kytnacode@gmail.com>
2025-07-29 15:19:01 -0700

    Add swaync

--- home/services/default.nix
+++ home/services/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hypridle.nix
     ./hyprpaper
+    ./swaync.nix
   ];
 
   # Too small to have a dedicated file:
---
+++ home/services/swaync.nix
@@ -0,0 +1,14 @@
+{
+  services.swaync = {
+    enable = true;
+
+    settings = {
+      control-center-margin-top = 10;
+      control-center-margin-bottomm = 10;
+      control-center-margint-right = 8;
+      control-center-layer = "overlay";
+      timeout = 7;
+      timeout-critical = 25;
+    };
+  };
+}
