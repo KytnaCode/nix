9f5b643c832e602489384b30eb68d847c1ee542a
kytnacode <kytnacode@gmail.com>
2025-09-25 07:33:28 -0700

    Clean and comment configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -32,48 +32,24 @@
   # Set your time zone.
   time.timeZone = "America/Mazatlan";
 
-  programs = {
-    zsh.enable = true;
-    hyprland = {
-      enable = true;
-      withUWSM = true;
-    };
-  };
-
+  # Hardware config.
   hardware = {
     graphics.enable = true;
 
+    # Use propetiary drivers.
     nvidia.open = false;
   };
 
   environment.pathsToLink = ["/share/zsh"];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
   services = {
+    # Connfigure Audio.
     pipewire = {
       enable = true;
       pulse.enable = true;
     };
 
+    # NVidia Propetiary drivers.
     xserver.videoDrivers = ["nvidia"];
 
     desktopManager.plasma6.enable = true;
@@ -84,8 +60,14 @@
     };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  programs = {
+    zsh.enable = true;
+
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.alex = {
@@ -93,39 +75,6 @@
     extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
   };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
