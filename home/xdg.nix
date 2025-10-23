7e1b4f33d1f07fbc6b8acc557d56deb38713183d
kytnacode <kytnacode@gmail.com>
2025-10-23 14:58:32 -0700

    Set Edge as default browser

--- home/xdg.nix
+++ home/xdg.nix
@@ -10,4 +10,25 @@
       terminal = false;
     };
   };
+
+  xdg.mime.enable = true;
+
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "com.microsoft.Edge.desktop";
+      "x-scheme-handler/http" = "com.microsoft.Edge.desktop";
+      "x-scheme-handler/https" = "com.microsoft.Edge.desktop";
+      "x-scheme-handler/about" = "com.microsoft.Edge.desktop";
+      "x-scheme-handler/unknown" = "com.microsoft.Edge.desktop";
+      "x-scheme-handler/chrome" = "com.microsoft.Edge.desktop";
+      "application/x-extension-htm" = "com.microsoft.Edge.desktop";
+      "application/x-extension-html" = "com.microsoft.Edge.desktop";
+      "application/x-extension-shtml" = "com.microsoft.Edge.desktop";
+      "application/xhtml+xml" = "com.microsoft.Edge.desktop";
+      "application/x-extension-xhtml" = "com.microsoft.Edge.desktop";
+      "application/x-extension-xht" = "com.microsoft.Edge.desktop";
+    };
+  };
 }
