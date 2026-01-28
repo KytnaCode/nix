36a23356a37f54a5d5b0b96867e661b209f5226b
kytnacode <kytnacode@gmail.com>
2026-01-28 16:14:14 -0700

    Integrate neo-tree with snacks rename

--- home/programs/neovim/plugins/neo-tree-nvim.lua
+++ home/programs/neovim/plugins/neo-tree-nvim.lua
@@ -1,3 +1,13 @@
+local function on_move(data)
+  Snacks.rename.on_rename_file(data.source, data.destination)
+end
+
+local events = require("neo-tree.events")
+
 require("neo-tree").setup({
   source_selector = { winbar = true },
+  event_handlers = {
+    { event = events.FILE_MOVED, handler = on_move },
+    { event = events.FILE_RENAMED, handler = on_move },
+  },
 })
