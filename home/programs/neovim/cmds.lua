f367dc9b737bdc805951e8d595d0d0c0e9f0e2fc
kytnacode <kytnacode@gmail.com>
2026-01-28 17:18:22 -0700

    Rename and fix DapExit command

--- home/programs/neovim/cmds.lua
+++ home/programs/neovim/cmds.lua
@@ -3,8 +3,8 @@ local cmd = api.nvim_create_user_command
 
 local dap, dapui = require("dap"), require("dapui")
 
-cmd("DapTerminate", function()
-  dap.termiante()
+cmd("DapExit", function()
+  dap.terminate()
   dapui.close()
 end, {
   desc = "terminate dap session and close dap ui",
