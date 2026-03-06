e668c3cbd19f76678e94fa334a02290691543215
kytnacode <kytnacode@gmail.com>
2026-03-06 09:49:18 -0700

    Automatically import programs

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,18 +1,7 @@
-{
-  imports = [
-    ./anki.nix
-    ./bat.nix
-    ./cava.nix
-    ./direnv.nix
-    ./fzf.nix
-    ./gh.nix
-    ./git.nix
-    ./hyprland
-    ./kitty.nix
-    ./neovim
-    ./tmux.nix
-    ./wofi.nix
-    ./zoxide.nix
-    ./zsh.nix
-  ];
+{lib, ...}:
+with lib; let
+  programs = filterAttrs (name: value: name != "default.nix") (builtins.readDir ./.);
+  imports = traceValSeq (mapAttrsToList (name: _: ./. + "/${name}") programs);
+in {
+  inherit imports;
 }
