a49ed6571291a32ed36993e69c18974c88dc1887
kytnacode <kytnacode@gmail.com>
2026-03-31 14:57:00 -0700

    Update catppuccin integration for lualine

--- home/programs/neovim/plugins/catppuccin-nvim.lua
+++ home/programs/neovim/plugins/catppuccin-nvim.lua
@@ -1,3 +1,5 @@
+local C = require("catppuccin.palettes").get_palette("mocha")
+
 require("catppuccin").setup({
   flavour = "latte",
   integrations = {
@@ -17,6 +19,41 @@ require("catppuccin").setup({
       enabled = true,
     },
     treesitter = true,
+    lualine = {
+      normal = {
+        a = { bg = C.blue, fg = C.mantle, gui = "bold" },
+        b = { bg = C.surface0, fg = C.blue },
+        c = { bg = C.mantle, fg = C.text },
+      },
+
+      insert = {
+        a = { bg = C.green, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.green },
+      },
+
+      terminal = {
+        a = { bg = C.green, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.green },
+      },
+
+      command = {
+        a = { bg = C.peach, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.peach },
+      },
+      visual = {
+        a = { bg = C.mauve, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.mauve },
+      },
+      replace = {
+        a = { bg = C.red, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.red },
+      },
+      inactive = {
+        a = { bg = C.mantle, fg = C.blue },
+        b = { bg = C.mantle, fg = C.surface1, gui = "bold" },
+        c = { bg = C.mantle, fg = C.overlay0 },
+      },
+    },
   },
 })
 
